29648c9a5937a96c098b92e96030631b7eb4186f
tukva <vanton@KH6805.softservecom.com>
2019-11-20 14:30:13 +0200

    Change config.py

--- server/app.py
+++ server/app.py
@@ -1,9 +1,10 @@
 from sanic import Sanic
 
+from config import SSO_API_HOST
 from routes import add_routes
 
 app = Sanic(name=__name__)
 add_routes(app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=SSO_API_HOST)
